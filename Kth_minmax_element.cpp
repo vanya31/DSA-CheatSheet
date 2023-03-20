2e6a898e730d885fcbcd7a9024a58983f3215f4b
Vanya Verma <72427243+vanya31@users.noreply.github.com>
2023-03-20 10:16:50 +0530

    Create Kth_minmax_element.cpp

---
+++ Kth_minmax_element.cpp
@@ -0,0 +1,30 @@
+// Time Complexity: O(nlogn)
+// Space Complexity: 
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        };
+
+        int k;
+        cin >> k;
+        sort(a, a + n);
+        cout << a[k - 1] << endl;
+    }
+
+    return 0;
+}
